f7edb9988e5be1751cbcfe5bd1aa8b83b7b27b36
Ryan Honeyager <honeyage@ucar.edu>
2020-12-29 16:40:48 -0500

    Added oops. Should add jedi base environment.

---
+++ jedi/packages/oops/package.py
@@ -0,0 +1,34 @@
+# Copyright 2013-2020 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: Apache-2.0
+
+import os
+from spack import *
+
+class Oops(CMakePackage):
+    """Object Oriented Prediction System"""
+
+    homepage = "https://github.com/JCSDA-internal/oops"
+    git = "git@github.com:JCSDA-internal/oops.git"
+    url = "https://github.com/JCSDA-internal/oops/archive/1.0.0.zip"
+
+    maintainers = ['rhoneyager', 'mmiesch']
+
+    version('master', branch='master')
+    version('develop', branch='develop', preferred=True)
+    version('1.0.0', commit='40e85e4772d395ea3df2ed29ad660f35c0d7dcb5')
+
+    #depends_on('cmake @3.10:', type=('build', 'run', 'link'))
+    depends_on('ecbuild', type=('build'))
+    depends_on('eckit')
+    depends_on('fckit')
+    depends_on('atlas')
+    depends_on('jedi_base')
+
+    def cmake_args(self):
+        res = [] 
+        res.append('-DCMAKE_MODULE_PATH=' + self.spec['ecbuild'].prefix + '/share/ecbuild/cmake')
+        #res.append('-DCMAKE_MODULE_PATH='+os.environ['CMAKE_MODULE_PATH'])
+        return res
+
