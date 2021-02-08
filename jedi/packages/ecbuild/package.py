45626722aa0a12e25e3cbd797717f8e810b079ef
Ryan Honeyager <honeyage@ucar.edu>
2021-02-08 17:39:40 -0500

    Updated ecbuild to 3.5.0

--- jedi/packages/ecbuild/package.py
+++ jedi/packages/ecbuild/package.py
@@ -14,6 +14,7 @@ class Ecbuild(CMakePackage):
 
     maintainers = ['rhoneyager', 'mmiesch']
 
+    version('3.5.0', commit='70f224596cf0058567bed2021bec0481742f9f63')
     version('3.4.1', commit='d4347cf92466c742ef989444b2acbf7c867a338e')
     version('3.4.0', commit='d18a2dfa4a350881bfdded080880eaa47d2361f8')
     version('3.3.5', commit='b10ab33fff852411fe842c4178d35993223e116f')
