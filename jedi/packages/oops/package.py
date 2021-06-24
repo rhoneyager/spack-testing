af82c38722fa01683301bf076fc07e6f71638d40
Ryan Honeyager <honeyage@ucar.edu>
2021-06-24 17:01:04 -0400

    Testing urls

--- jedi/packages/oops/package.py
+++ jedi/packages/oops/package.py
@@ -15,10 +15,10 @@ class Oops(CMakePackage):
 
     maintainers = ['rhoneyager', 'mmiesch']
 
-    version('master', branch='master', no_cache=True)
+    version('master', branch='master', no_cache=True, url="https://github.com/JCSDA/oops.git")
     version('develop', branch='develop', preferred=True, no_cache=True)
-    version('1.1.0', commit='0a07af4672732aed1d2a279519670064696bcfbe')
-    version('1.0.0', commit='40e85e4772d395ea3df2ed29ad660f35c0d7dcb5')
+    version('1.1.0', commit='0a07af4672732aed1d2a279519670064696bcfbe', url="https://github.com/JCSDA/oops.git")
+    version('1.0.0', commit='40e85e4772d395ea3df2ed29ad660f35c0d7dcb5', url="https://github.com/JCSDA/oops.git")
 
     #depends_on('cmake @3.10:', type=('build', 'run', 'link'))
     depends_on('ecbuild', type=('build'))
