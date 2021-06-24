9d8ff05718271c50254d8ccc828b99a7a6764950
Ryan Honeyager <honeyage@ucar.edu>
2021-06-24 10:37:44 -0400

    Added config file copy. Moved spack path. Addressed engines-env points.

--- jedi/packages/jedi-cmake/package.py
+++ jedi/packages/jedi-cmake/package.py
@@ -10,13 +10,14 @@ class JediCmake(CMakePackage):
     """CMake/ecbuild toolchains to facilitate portability on different systems."""
 
     homepage = "https://github.com/JCSDA-internal/jedi-cmake"
-    git = "git@github.com:JCSDA-internal/jedi-cmake.git"
-    url = "https://github.com/JCSDA-internal/jedi-cmake/archive/1.0.0.zip"
+    git = "https://github.com/JCSDA/jedi-cmake.git"
+    url = "https://github.com/JCSDA/jedi-cmake/archive/1.0.0.zip"
 
     maintainers = ['rhoneyager', 'mmiesch']
 
     version('master', branch='master', no_cache=True)
-    version('develop', branch='develop', preferred=True, no_cache=True)
+    version('develop', branch='develop', no_cache=True)
+    version('1.1.0', commit='4479d1554cd4ef77fc4cefaca8537090359f8654', preferred=True)
     version('1.0.0', commit='3f59be473037bd3d21df90c7a3f200d8012f679e')
 
     depends_on('cmake @3.10:', type=('build'))
